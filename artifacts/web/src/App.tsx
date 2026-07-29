69ef6f17749f8e2a81a6978567f8892e76eaf127
wanderworldvlogs0-arch <wanderworldvlogs0@gmail.com>
2026-07-29 09:07:38 +0530

    Update App.tsx

--- artifacts/web/src/App.tsx
+++ artifacts/web/src/App.tsx
@@ -5,6 +5,7 @@ import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Dashboard } from "./pages/Dashboard";
 import { Subjects } from "./pages/Subjects";
+import { Quiz } from "./pages/Quiz";
 import { Videos } from "./pages/Videos";
 import { Pdfs } from "./pages/Pdfs";
 
