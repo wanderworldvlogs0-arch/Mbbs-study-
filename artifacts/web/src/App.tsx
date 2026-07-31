c69053b6763225266af0833b1440d02d60600291
wanderworldvlogs0-arch <wanderworldvlogs0@gmail.com>
2026-07-31 16:33:17 +0530

    Update App.tsx

--- artifacts/web/src/App.tsx
+++ artifacts/web/src/App.tsx
@@ -14,6 +14,7 @@ import { Progress } from "./pages/Progress";
 import { Rewards } from "./pages/Rewards";
 import { Settings } from "./pages/Settings";
 import { Profile } from "./pages/Profile";
+import { SearchPage } from "./pages/Search";
 
 function ComingSoon() {
   return (
